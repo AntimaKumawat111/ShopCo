d98351c52b895a5b8c23683dece5ca1bec9dbd94
antima kumawat <aktank810@gmail.com>
2026-02-07 12:27:12 +0530

    create checkout screen

--- src/screens/Checkout/Checkout.tsx
+++ src/screens/Checkout/Checkout.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import BaseLayout from '../../components/BaseLayout';
 import Header from '../../components/header';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {useTheme} from '../../helper/color';
 
 function Checkout() {
+  const theme = useTheme();
+  const styles = createStyles(theme);
+
   return (
     <>
       <BaseLayout>
         <Header title="Cart" />
+        <ScrollView>
+          <View style={styles.container}>
+            <Text>hello</Text>
+          </View>
+        </ScrollView>
       </BaseLayout>
     </>
   );
 }
 
 export default Checkout;
+
+const createStyles = (theme: any) => {
+  return StyleSheet.create({
+    container: {},
+  });
+};
